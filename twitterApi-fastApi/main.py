fa0736cc5a42030c9cbcc48123917edd4f784580
Lokochris <joseluismanco37@gmail.com>
2022-12-15 02:44:53 -0500

    models

---
+++ twitterApi-fastApi/main.py
@@ -0,0 +1,37 @@
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+#Fast API
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#MODELS
+class UserBase(BaseModel):
+    user_id : UUID = Field()
+    mail : EmailStr = Field()
+
+class UserLogin(UserBase):
+    password : str  = Field(..., 
+                            min_length=8)
+
+class User(UserBase):
+    username : str = Field(
+        ...,
+        min_length=1,
+        max_length=20
+    )
+    birth_date: Optional[date] = Field(default=None)
+class Tweet(BaseModel):
+    pass
+
+
+@app.get(path="/")
+def home():
+    return {"Twitter API" : "Working!"}
